0789e6e60a103ca9f34ecfbebedf1c81aa4aeb69
haihoangvu1809 <0306191215@caothang.edu.vn>
2022-01-23 09:37:16 +0700

    Testdemo

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Vietnam tourist',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
       home: Scaffold(
         appBar: AppBar(
